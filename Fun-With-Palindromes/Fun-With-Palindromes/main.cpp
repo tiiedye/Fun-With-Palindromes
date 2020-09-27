8475613335bed63aaf53b2fc2a7a5d26c85cf138
tiiedye <tiiedye@gmail.com>
2020-09-27 11:04:18 -0700

    creating the is_palindrome bool function

--- Fun-With-Palindromes/Fun-With-Palindromes/main.cpp
+++ Fun-With-Palindromes/Fun-With-Palindromes/main.cpp
@@ -8,7 +8,32 @@
 bool is_palindrome(const std::string& s) {
     // You must implement this function.
     // Since we are learning the STL - use a deque to solve the problem.
-    return false;
+    std::deque<char> deque_char;
+
+    for (char c : s) {
+        if (std::isalpha(c)) {
+            deque_char.push_back(std::toupper(c));
+        }
+    }
+
+    char char1{};
+    char char2{};
+
+    // loop to compare front and back characters of the string.
+    while (deque_char.size() > 1) {
+        char1 = deque_char.front();
+        char2 = deque_char.back();
+        deque_char.pop_front();
+        deque_char.pop_back();
+
+        if (char1 != char2) {
+            return false;
+            // returns false and ends loop if characters do not match at any point through the loop.
+        }
+    }
+
+    // returns true if the loop goes all the way through the string and does not return false at any point.
+    return true;
 }
 
 int main() {
